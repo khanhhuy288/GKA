73300bf4b2f1abc514b76580f2b3df2603c9118b
Huy Tran <khanhhuy288@gmail.com>
2018-11-01 03:17:19 +0100

    Create GkaGraph Class.

--- GKA1/src/GkaGraph.java
+++ GKA1/src/GkaGraph.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.UUID;
 
+import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
 
 public class GkaGraph extends MultiGraph {
@@ -9,9 +11,27 @@ public class GkaGraph extends MultiGraph {
 		super(id);
 		this.nodeNameToIdMap = new HashMap<String, String>(); 
 	}
-
+	
 	public HashMap<String, String> getNodeNameToIdMap() {
 		return nodeNameToIdMap;
 	}
+
+	public String getNodeId(String nodeName) {
+		String nodeId;
+		if (!nodeNameToIdMap.containsKey(nodeName)) {
+			nodeId = createStringId();
+			this.addNode(nodeId);
+			Node node1 = this.getNode(nodeId);
+			node1.addAttribute("name", nodeName);
+			nodeNameToIdMap.put(nodeName, nodeId);
+		} else {
+			nodeId = nodeNameToIdMap.get(nodeName);
+		}
+		return nodeId;
+	}
+	
+	public static String createStringId() {
+		return UUID.randomUUID().toString();
+	} 
 	
 }
--- GKA1/src/GkaReader.java
+++ GKA1/src/GkaReader.java
@@ -1,10 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Scanner;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,79 +15,65 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
 
+import sun.font.CreatedFontTracker;
+
 public class GkaReader {
-	public static void main(String[] args) throws MalformedURLException, IOException {
-		String filename = "graph06.gka";
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		String filename = "graph01.gka";
 		Path path = Paths.get(System.getProperty("user.dir"), "/gka-Dateien/" + filename);
-		Scanner input = new Scanner(new URL("file:///" + path.toAbsolutePath()).openStream(), "UTF-8");
+		File file = new File(path.toString());
 		String regex = "^\\s*([\\wÄäÖöÜüß]+)\\s*((->|--)\\s*([\\wÄäÖöÜüß]+)\\s*(\\(\\s*([\\wÄäÖöÜüß]+)\\s*\\)\\s*)?(:\\s*(\\d+)\\s*)?)?;$";
 		Pattern pattern = Pattern.compile(regex);
 
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-		Graph graph = new MultiGraph("Tutorial 1");
+		GkaGraph graph = new GkaGraph("My Graph");
 
 		graph.addAttribute("ui.stylesheet", "url('file:///D:/GitHub/GKA/GKA1/src/stylesheet')");
 		graph.display();
 
-		HashMap<String, String> nodeNameNodeIdMap = new HashMap<String, String>();
+		HashMap<String, String> nodeNameNodeIdMap = graph.getNodeNameToIdMap();
 
-		while (input.hasNextLine()) {
-			String line = input.nextLine();
-			Matcher matcher = pattern.matcher(line);
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				Matcher matcher = pattern.matcher(line);
 
-			if (matcher.matches()) {
-				String nameNode1 = matcher.group(1);
-				String direction = matcher.group(3);
-				String nameNode2 = matcher.group(4);
-				String edgeName = matcher.group(6);
-				String edgeWeight = matcher.group(8);
+				if (matcher.matches()) {
+					String nameNode1 = matcher.group(1);
+					String direction = matcher.group(3);
+					String nameNode2 = matcher.group(4);
+					String edgeName = matcher.group(6);
+					String edgeWeight = matcher.group(8);
 
-				String node1_ID;
-				if (!nodeNameNodeIdMap.containsKey(nameNode1)) {
-					node1_ID = UUID.randomUUID().toString();
-					graph.addNode(node1_ID);
-					Node node1 = graph.getNode(node1_ID);
-					node1.addAttribute("name", nameNode1);
-					nodeNameNodeIdMap.put(nameNode1, node1_ID);
-				} else {
-					node1_ID = nodeNameNodeIdMap.get(nameNode1);
+					String node1_ID = graph.getNodeId(nameNode1);
+
+					if (nameNode2 != null) {
+						String node2_ID = graph.getNodeId(nameNode2);
+						String edgeId = GkaGraph.createStringId();
+
+						if (direction.equals("--")) {
+							graph.addEdge(edgeId, node1_ID, node2_ID);
+
+						} else if (direction.equals("->")) {
+							graph.addEdge(edgeId, node1_ID, node2_ID, true);
+						}
+
+						Edge edge = graph.getEdge(edgeId);
+
+						if (edgeName != null) {
+							edge.addAttribute("name", edgeName);
+						}
+
+						if (edgeWeight != null) {
+							edge.addAttribute("weight", Integer.valueOf(edgeWeight));
+						}
+					}
+
 				}
-
-				if (nameNode2 != null) {
-					String node2_ID;
-					if (!nodeNameNodeIdMap.containsKey(nameNode2)) {
-						node2_ID = UUID.randomUUID().toString();
-						graph.addNode(node2_ID);
-						Node node2 = graph.getNode(node2_ID);
-						node2.addAttribute("name", nameNode2);
-						nodeNameNodeIdMap.put(nameNode2, node2_ID);
-					} else {
-						node2_ID = nodeNameNodeIdMap.get(nameNode2);
-					}
-
-					String edgeId = UUID.randomUUID().toString();
-
-					if (direction.equals("--")) {
-						graph.addEdge(edgeId, node1_ID, node2_ID);
-
-					} else if (direction.equals("->")) {
-						graph.addEdge(edgeId, node1_ID, node2_ID, true);
-					}
-
-					Edge edge = graph.getEdge(edgeId);
-
-					if (edgeName != null) {
-						edge.addAttribute("name", edgeName);
-					}
-
-					if (edgeWeight != null) {
-						edge.addAttribute("weight", Integer.valueOf(edgeWeight));
-					}
-				}
-
 			}
 		}
 
+		// add labels for nodes and edges
 		for (Node node : graph) {
 			node.addAttribute("label", (String) node.getAttribute("name"));
 		}
@@ -101,9 +88,6 @@ public class GkaReader {
 			}
 		}
 
-		input.close();
-
 	}
-	
-	
+
 }
