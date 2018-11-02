904f97eab14440bfe15795a4a3f9cbd0febfaf48
Huy Tran <khanhhuy288@gmail.com>
2018-11-02 22:19:53 +0100

    Add documentation and comments for GkaUtils and GkaGraph.

--- GKA1/src/GkaGraph.java
+++ GKA1/src/GkaGraph.java
@@ -5,20 +5,47 @@ import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
 
+/**
+ * GkaGraph has useful functions for working with graph's properties.
+ * 
+ * @author Huy Tran PC
+ *
+ */
 public class GkaGraph extends MultiGraph {
-	private HashMap<String, String> nodeNameToIdMap;	
-	
+	// hash map with node names as keys and node ids as values
+	private HashMap<String, String> nodeNameToIdMap;
+
+	/**
+	 * Constructor for a GkaGraph.
+	 * 
+	 * @param id
+	 *            Id of the graph
+	 */
 	public GkaGraph(String id) {
 		super(id);
-		this.nodeNameToIdMap = new HashMap<String, String>(); 
+		this.nodeNameToIdMap = new HashMap<String, String>();
 	}
-	
+
+	/**
+	 * Get the hash map with node names as keys and node ids as values.
+	 * 
+	 * @return The hash map
+	 */
 	public HashMap<String, String> getNodeNameToIdMap() {
 		return nodeNameToIdMap;
 	}
 
+	/**
+	 * Get the id of a node from the hash map. <br>
+	 * If name is new, create a new node.
+	 * 
+	 * @param nodeName
+	 *            Name of a node
+	 * @return Id of that node.
+	 */
 	public String getNodeId(String nodeName) {
 		String nodeId;
+
 		if (!nodeNameToIdMap.containsKey(nodeName)) {
 			nodeId = createStringId();
 			this.addNode(nodeId);
@@ -28,50 +55,88 @@ public class GkaGraph extends MultiGraph {
 		} else {
 			nodeId = nodeNameToIdMap.get(nodeName);
 		}
+
 		return nodeId;
 	}
-	
+
+	/**
+	 * Find all edges with properties and get their string presentation in GKA
+	 * format.
+	 * 
+	 * @return String presentation in GKA format of all edges and their
+	 *         properties.
+	 */
 	public String getEdgesString() {
-		String str = ""; 
-		
+		String str = "";
+
 		for (Edge edge : this.getEachEdge()) {
+			// add name of node 1
 			str += edge.getNode0().getAttribute("name");
-			
+
+			// add direction
 			if (edge.isDirected()) {
 				str += " -> " + edge.getNode1().getAttribute("name");
 			} else {
 				str += " -- " + edge.getNode1().getAttribute("name");
 			}
-			
+
+			// add name of edge
 			if (edge.hasAttribute("name")) {
 				str += " (" + edge.getAttribute("name") + ")";
 			}
-			
+
+			// add weight of edge
 			if (edge.hasAttribute("weight")) {
 				str += " : " + edge.getAttribute("weight");
 			}
-			
+
 			str += ";\n";
 		}
-		
+
 		return str;
 	}
-	
+
+	/**
+	 * Find single nodes and get their string presentation in GKA format.
+	 * 
+	 * @return String presentation in GKA format of all single nodes.
+	 */
 	public String getSingleNodesString() {
 		String str = "";
-	
+
 		for (Node node : this.getEachNode()) {
-			if(node.getDegree() == 0){
-	            str += node.getAttribute("name") + ";\n";
-	        }
+			if (node.getDegree() == 0) {
+				str += node.getAttribute("name") + ";\n";
+			}
 		}
-		
+
 		return str;
 	}
-	
+
+	/**
+	 * Add an edge to the graph.
+	 * @param edgeId Id of the edge
+	 * @param nodeId1 Id of node 1 
+	 * @param nodeId2 Id of node 2  
+	 * @param direction Direction on the edge
+	 */
+	public void addEdge(String edgeId, String nodeId1, String nodeId2, String direction) {
+		if (direction.equals("--")) {
+			this.addEdge(edgeId, nodeId1, nodeId2);
+		} else if (direction.equals("->")) {
+			this.addEdge(edgeId, nodeId1, nodeId2, true);
+		}
+	}
+
+	/**
+	 * Add the properties on an edge. 
+	 * @param edgeId Id of the edge
+	 * @param edgeName Name of the edge
+	 * @param edgeWeight Weight of the edge
+	 */
 	public void addEdgeAttr(String edgeId, String edgeName, String edgeWeight) {
 		Edge edge = this.getEdge(edgeId);
-		
+
 		if (edgeName != null) {
 			edge.addAttribute("name", edgeName);
 		}
@@ -80,13 +145,17 @@ public class GkaGraph extends MultiGraph {
 			edge.addAttribute("weight", Integer.valueOf(edgeWeight));
 		}
 	}
-	
+
+	/**
+	 * Add labels to nodes and edges.
+	 */
 	public void addLabels() {
 		for (Node node : this) {
 			node.addAttribute("label", (String) node.getAttribute("name"));
 		}
 
 		for (Edge edge : this.getEachEdge()) {
+			// ??? labels for name is overwritten by labels for weight
 			if (edge.hasAttribute("name")) {
 				edge.addAttribute("label", edge.getAttribute("name").toString());
 			}
@@ -96,9 +165,13 @@ public class GkaGraph extends MultiGraph {
 			}
 		}
 	}
-	
+
+	/**
+	 * Create a unique string id used for creating graph, node, edge objects.
+	 * @return A unique string id. 
+	 */
 	public static String createStringId() {
 		return UUID.randomUUID().toString();
-	} 
-	
+	}
+
 }
--- GKA1/src/GkaUtils.java
+++ GKA1/src/GkaUtils.java
@@ -8,66 +8,92 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.graphstream.graph.Edge;
-import org.graphstream.graph.Node;
-
+/**
+ * Utility Class for reading a GKA file or saving a GkaGraph object as GKA
+ * file.<br>
+ * GKA format of each line: <br>
+ * <strong>directed</strong>: &lt;name node 1&gt; [ -> &lt;name node 2&gt;
+ * [(&lt;edge name&gt;)] [: &lt;edge weight&gt;]]<br>
+ * <strong>undirected</strong>: &lt;name node 1&gt; [ -- &lt;name node 2&gt;
+ * [(&lt;edge name&gt;)] [: &lt;edge weight&gt;]]<br>
+ * 
+ * @author Huy Tran PC
+ *
+ */
 public class GkaUtils {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		String filename = "graph07.gka";
-		GkaGraph myGraph = GkaUtils.read(filename);
+		String filename = "graph04.gka";
+		GkaGraph myGraph = GkaUtils.read(filename, "defaultStylesheet");
 		myGraph.display();
 		GkaUtils.save(myGraph, "holy.gka");
 	}
 
 	/**
-	 * Read a gka file and return a GkaGraph object.
+	 * Read a GKA file from the <em>gka-Dateien</em> folder. Default stylesheet
+	 * in the <em>stylesheet</em> folder is used.
 	 * 
-	 * @param filePath
-	 * @return
+	 * @param filename
+	 *            Name of the GKA file
+	 * @return A GkaGraph object
+	 * @throws UnsupportedEncodingException
+	 *             if the encoding is not supported
 	 * @throws FileNotFoundException
+	 *             if the file path cannot be found
 	 * @throws IOException
+	 *             if an I/O exception occurs
 	 */
-	public static GkaGraph read(String filename) throws FileNotFoundException, IOException {
-		Path stylesheetPath = Paths.get(System.getProperty("user.dir") + "/stylesheet/defaultStylesheet");
-		return read(filename, stylesheetPath);
+	public static GkaGraph read(String filename)
+			throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		String stylesheetName = "defaultStylesheet";
+		return read(filename, stylesheetName);
 	}
 
 	/**
-	 * Read a gka file and return a GkaGraph object.
+	 * Read a GKA file from the <em>gka-Dateien</em> folder. Custom stylesheet
+	 * in the <em>stylesheet</em> folder is used.
 	 * 
-	 * @param filePath
-	 * @param stylesheetPath
-	 * @return
+	 * @param filename
+	 *            Name of the GKA file
+	 * @param stylesheetName
+	 *            Name of the stylesheet.
+	 * @return A GkaGraph object
 	 * @throws UnsupportedEncodingException
+	 *             if the encoding is not supported
 	 * @throws FileNotFoundException
+	 *             if the file path cannot be found
 	 * @throws IOException
+	 *             if an I/O exception occurs
 	 */
-	public static GkaGraph read(String filename, Path stylesheetPath)
+	public static GkaGraph read(String filename, String stylesheetName)
 			throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		// create empty graph
 		GkaGraph graph = new GkaGraph(GkaGraph.createStringId());
 
-		if (stylesheetPath != null) {
-			graph.addAttribute("ui.stylesheet", "url('file:///" + stylesheetPath.toString() + "')");
-		}
-
+		// set viewer
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
+		// set stylesheet
+		String stylesheetPath = System.getProperty("user.dir") + "/stylesheet/" + stylesheetName;
+		graph.addAttribute("ui.stylesheet", "url('file:///" + stylesheetPath + "')");
+
+		// regex for GKA format of each line
 		String regex = "^\\s*([\\wÄäÖöÜüß]+)\\s*((->|--)\\s*([\\wÄäÖöÜüß]+)\\s*(\\(\\s*([\\wÄäÖöÜüß]+)\\s*\\)\\s*)?(:\\s*(\\d+)\\s*)?)?;$";
 		Pattern pattern = Pattern.compile(regex);
 
+		// set file path
 		String filePath = System.getProperty("user.dir") + "/gka-Dateien/" + filename;
 		File file = new File(filePath);
-		try (BufferedReader br = new BufferedReader(
-				new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+
+		// read file line by line
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
 			String line;
 			while ((line = br.readLine()) != null) {
 				Matcher matcher = pattern.matcher(line);
 
+				// find the properties of a line
 				if (matcher.matches()) {
 					String nameNode1 = matcher.group(1);
 					String direction = matcher.group(3);
@@ -75,19 +101,16 @@ public class GkaUtils {
 					String edgeName = matcher.group(6);
 					String edgeWeight = matcher.group(8);
 
+					// create or get node 1
 					String nodeId1 = graph.getNodeId(nameNode1);
 
 					if (nameNode2 != null) {
+						// create or get node 2
 						String nodeId2 = graph.getNodeId(nameNode2);
 
+						// create edge with its properties
 						String edgeId = GkaGraph.createStringId();
-
-						if (direction.equals("--")) {
-							graph.addEdge(edgeId, nodeId1, nodeId2);
-						} else if (direction.equals("->")) {
-							graph.addEdge(edgeId, nodeId1, nodeId2, true);
-						}
-
+						graph.addEdge(edgeId, nodeId1, nodeId2, direction);
 						graph.addEdgeAttr(edgeId, edgeName, edgeWeight);
 					}
 
@@ -102,7 +125,7 @@ public class GkaUtils {
 	}
 
 	/**
-	 * Save a GkaGraph object as gka file.
+	 * Save a GkaGraph object as a GKA file to the <em>gka-Dateien</em> folder.
 	 * 
 	 * @param graph
 	 * @throws IOException
@@ -111,17 +134,21 @@ public class GkaUtils {
 	 */
 	public static void save(GkaGraph graph, String filename)
 			throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		// set save path
 		String filePath = System.getProperty("user.dir") + "/gka-Dateien/" + filename;
 		File file = new File(filePath.toString());
-		try (BufferedWriter writer = new BufferedWriter(
-				new OutputStreamWriter(new FileOutputStream(file), "UTF-8"))) {
+
+		// write GKA object to file
+		try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"))) {
 			String str = "";
-			
+
+			// add string of all edges 
 			str += graph.getEdgesString();
+			
+			// add string of single nodes
 			str += graph.getSingleNodesString();
-			
+
 			writer.write(str);
-			
 		}
 	}
 
